66769a16962b61404fcba12be7bf203c54f363c2
walde-dev <paninwaldemar@gmail.com>
2022-01-10 19:04:56 +0100

    added header + click /  tick logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import Header from "./components/Header";
+import "../styles/globals.css"
+import { useEffect, useRef, useState } from "react";
+import useInterval from "./components/Logic/Hooks/useInterval";
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [coins, setCoins] = useState(0)
+  const [coinsPerSecond, setCoinsPerSecond] = useState(0);
+  const [coinsPerClick, setCoinsPerClick] = useState(1);
+
+
+  function click() {
+    setCoins(coins + coinsPerClick);
+  }
+
+  function tick() {
+    console.log('TICK')
+    setCoins(coins + coinsPerSecond);
+  }
+
+  useInterval(() => {
+    tick();
+  }, 1000);
+ 
+
+
+
+  return (
+    <div
+      className='flex flex-col font-roboto font-light lg:px-32 md:px-16 py-7 bg-main-background-blue w-screen max-w-full max-h-full h-full min-h-screen'
+      onClick={() =>
+        click()
+      }>
+      <Header coins={coins} coinsPerSecond={coinsPerSecond} coinsPerClick={coinsPerClick} />
+    </div>
+  );
 }
 
 export default MyApp
---
+++ pages/components/Header.js
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+
+import Coins from "../../public/icons/coins.svg"
+import Clock from "../../public/icons/clock.svg"
+import Finger from "../../public/icons/finger.svg"
+import Mana from "../../public/icons/mana.svg"
+
+
+
+import { CogIcon, InformationCircleIcon, UserIcon, UsersIcon } from '@heroicons/react/solid'
+
+
+import TopMenuButton from './Buttons/TopMenuButton'
+import Logic from './Logic/logic'
+
+
+
+export default function Header(props) {
+
+    /* Mock Data */
+    const [coinAmount, setCoinAmount] = useState(3000)
+
+    return (
+        <div className='flex flex-row w-full sm:h-[75px] rounded-2xl px-7 bg-secondary-blue text-grey items-center'>
+
+            <div className='flex flex-row w-full justify-between '>
+                <div className='flex sm:flex-row flex-col justify-center sm:space-x-5 space-y-1'>
+                    <div className='flex flex-row space-x-1.5'>
+                        <Coins className='self-center sm:w-10 sm:h-10 w-7 h-7' />
+                        <div className='self-center'>
+                            {props.coins}
+                        </div>
+                    </div>
+
+                    <div className='flex flex-row space-x-1.5'>
+                        <Clock className='self-center w-7 h-7' />
+                        <div className='self-center'>
+                            {props.coinsPerSecond}
+                        </div>
+                    </div>
+
+                    <div className='flex flex-row space-x-1.5'>
+                        <Finger className='self-center w-7 h-7' />
+                        <div className='self-center'>
+                            {props.coinsPerClick}
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex  sm:flex-row flex-col-reverse justify-evenly sm:justify-between w-5/12 max-w-md'>
+                    <div className='flex flex-row self-center space-x-1.5'>
+                        <Mana className='self-center w-7 h-7' />
+                        <div className='self-center'>
+                            {coinAmount}
+                        </div>
+                    </div>
+
+                    <div className='flex flex-row justify-around self-center '>
+                        <TopMenuButton icon={UserIcon} />
+                        <TopMenuButton icon={InformationCircleIcon} />
+                        <TopMenuButton icon={CogIcon} />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
